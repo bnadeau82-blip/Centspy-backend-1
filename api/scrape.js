82133f85c2394c6ebe6928c70ba154a45f4ae005
bnadeau82-blip <b.nadeau82@gmail.com>
2026-06-01 22:27:51 -0500

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     const input = storeId
       ? {
           storeId: String(storeId),
-          maxResults: 200,
+          maxResults: 2000,
           parallelRequests: 3,
           proxyConfig: {
             useApifyProxy: true,
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         }
       : {
           zipcode: String(zipCode),
-          maxResults: 200,
+          maxResults: 2000,
           parallelRequests: 3,
           proxyConfig: {
             useApifyProxy: true,
