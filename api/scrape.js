9ca7250677c0e8f4f504f46e1a245714bb733a06
Brandon <cin@Brandons-MacBook-Air.local>
2026-06-22 04:12:10 -0500

    Read params from query string and body

--- api/scrape.js
+++ api/scrape.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const { storeId, zipCode, type } = req.body || {};
+const { storeId, zipCode, type } = { ...(req.query || {}), ...(req.body || {}) };
   const isClearance = type === 'clearance';
 
   try {
